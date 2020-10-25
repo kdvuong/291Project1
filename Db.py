e348dc5226c9545f0d0419bab55ea44cf5c39520
kdvuong <khangdieuvuong@gmail.com>
2020-10-25 14:11:38 -0600

    feat: implement log in and register

--- Db.py
+++ Db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date
 
 class Db:
     def __init__(self):
@@ -8,11 +9,42 @@ class Db:
         dbName = input("Enter db name: ")
         self.conn = sqlite3.connect(dbName + ".db")
 
+    def login(self, uid, password):
+        c = self.conn.cursor()
+        c.execute("SELECT * FROM users WHERE uid = :uid AND pwd = :password", {"uid": uid, "password": password})
+        user = c.fetchone()
+        if (user == None):
+            return False
+        else:
+            return True
+
+    def register(self, uid, name, password, city):
+        c = self.conn.cursor()
+        c.execute("SELECT uid FROM users WHERE uid = :uid", {"uid": uid})
+        existingUid = c.fetchone()
+        if (existingUid == None):
+            #create user
+            if (len(uid) > 4):
+                print("Uid must be less than 5 characters")
+                return False
+            else:
+                c.execute(
+                    """
+                        INSERT INTO users VALUES
+                        (:uid, :name, :password, :city, :date)
+                    """, {"uid": uid, "name": name, "password": password, "city": city, "date": date.today()}
+                )
+                self.conn.commit()
+                return True
+
+        else:
+            print("Uid already registered")
+            return False
+
     def getUsers(self):
         c = self.conn.cursor()
         c.execute("SELECT * FROM users")
         return c.fetchall()
 
-    
     def close(self):
         self.conn.close()
--- main.py
+++ main.py
@@ -5,6 +5,34 @@ db = Db()
 def main():
     db.setup()
     print(db.getUsers())
+    while (True):
+        isRegistered = input("Are you registered as a user?(T/F) ")
+        if isRegistered.lower() == "t":
+            # user name, password
+            uid = input("uid: ")
+            password = input("password: ")
+            loginSuccess = db.login(uid, password)
+            if (loginSuccess):
+                print("Logged in")
+                break
+            else:
+                print("Uid or password is wrong")
+        elif isRegistered.lower() == "f":
+            uid = input("uid: ")
+            password = input("password: ")
+            name = input("name: ")
+            city = input("city: ")
+
+            registerSuccess = db.register(uid, name, password, city)
+            if (registerSuccess):
+                print("Register success")
+            else:
+                print("Register fail")
+        else:
+            print("Invalid input, please choose T or F")
+
+    db.close()    
+
 
 if __name__ == "__main__":
     main()
