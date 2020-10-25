1bd7cfe78d04cb2f051e3347b7f601208e5de060
kdvuong <khangdieuvuong@gmail.com>
2020-10-25 14:28:28 -0600

    feat: implement program loop

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ from Db import Db
 
 db = Db()
 
+options = """-----------------------------------
+Choose the following option:
+-----------------------------------
+POST (post a question)
+SEARCH (search for a post)
+ANSWER (post answer to a question)
+VOTE (vote a post)
+LOGOUT (log out)
+------------------------------------
+What do you want to do? """
+
 def main():
     db.setup()
-    print(db.getUsers())
     while (True):
-        isRegistered = input("Are you registered as a user?(T/F) ")
-        if isRegistered.lower() == "t":
+        isRegistered = input("Are you registered as a user?(T/F) ").lower()
+        isLoggedIn = False
+        if isRegistered == "t":
             # user name, password
             uid = input("uid: ")
             password = input("password: ")
             loginSuccess = db.login(uid, password)
             if (loginSuccess):
                 print("Logged in")
-                break
+                isLoggedIn = True
             else:
                 print("Uid or password is wrong")
-        elif isRegistered.lower() == "f":
+        elif isRegistered == "f":
             uid = input("uid: ")
             password = input("password: ")
             name = input("name: ")
@@ -30,6 +41,21 @@ def main():
                 print("Register fail")
         else:
             print("Invalid input, please choose T or F")
+
+        while (isLoggedIn):
+            action = input(options).lower()
+
+            if (action == "post"):
+                print("Posting a question")
+            elif (action == "search"):
+                print("Searching a post")
+            elif (action == "answer"):
+                print("Answering a question")
+            elif (action == "vote"):
+                print("Voting a post")
+            elif (action == "logout"):
+                isLoggedIn = False
+                break
 
     db.close()    
 
